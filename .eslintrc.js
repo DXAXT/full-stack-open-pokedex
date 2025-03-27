4dcb76f3fa4247d60dacfee651ec7f95b09717b8
DXAXT <davidxtsoi@gmail.com>
2025-03-27 12:22:46 +0100

    11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
@@ -14,6 +15,11 @@ module.exports = {
     },
     "ecmaVersion": 2018,
     "sourceType": "module"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
   },
   "plugins": [
     "react", "jest"
